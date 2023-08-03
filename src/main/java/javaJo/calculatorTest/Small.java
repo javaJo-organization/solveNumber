19603096f517ab066dbc54a67746744382448340
costudying <bnj021212@gmail.com>
2023-08-04 08:58:57 +0900

    최종 통합본

--- src/main/java/javaJo/calculatorTest/Small.java
+++ src/main/java/javaJo/calculatorTest/Small.java
@@ -1,22 +1,24 @@
 package javaJo.calculatorTest;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Small {
     public static void weak() {
-            Scanner sc = new Scanner(System.in);
-            String a,b;
-            System.out.println("비교하고 싶은 첫 번째 숫자를 입력해 주세요.");
-            a = sc.nextLine();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("비교하고 싶은 첫 번째 숫자를 입력해 주세요.");
+        int a = Integer.parseInt(sc.next());
 
-            System.out.println("비교하고 싶은 두 번째 숫자를 입력해 주세요.");
-            b = sc.nextLine();
+        System.out.println("비교하고 싶은 두 번째 숫자를 입력해 주세요.");
+        int b = Integer.parseInt(sc.next());
 
-            int x = Integer.parseInt(a);
-            int y = Integer.parseInt(b);
 
-        if(x>y) System.out.println(">");
-        else if(x<y) System.out.println("<");
-        else System.out.println("==");
+        if (a > b) {
+            System.out.println(b + "가" + a + "보다 작습니다.");
+        } else if (a < b) {
+            System.out.println(a + "가" + b + "보다 작습니다.");
+        } else if (a == b) {
+            System.out.println(a + "와" + b + "는 같습니다.");
+        }
     }
 }
--- src/main/java/javaJo/calculatorTest/TimesTable.java
+++ src/main/java/javaJo/calculatorTest/TimesTable.java
@@ -5,11 +5,10 @@ public class TimesTable {
     public static void timesTable()   {
         Scanner sc = new Scanner(System.in);
         System.out.println("출력을 원하는 구구단은 몇 단인가요?(숫자만 입력 가능합니다.)");
-        int x = sc.nextInt();
+        int x = Integer.parseInt(sc.next());
         System.out.println("구구단 " + x + "단의 값은 다음과 같습니다.");
-        for( int i = 1 ; i < 10 ; ++i ) {
+        for( int i = 1 ; i < 10 ; i++ ) {
             System.out.println(x + "*" + i + "=" + x * i);
         }
-        sc.close();
     }
 }
